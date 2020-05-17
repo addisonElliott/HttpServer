aa4f34ae4905d50bdfefecaf467d7361082cf8b5
Addison Elliott <addison.elliott@gmail.com>
2020-05-17 10:46:28 -0500

    Fix errors

--- src/httpServer/middleware/CORS.cpp
+++ src/httpServer/middleware/CORS.cpp
@@ -5,16 +5,16 @@ namespace middleware
 
 HttpPromise CORS(std::shared_ptr<HttpData> data)
 {
-    response->setHeader("Access-Control-Allow-Origin", request->headerDefault("Origin", "*"));
-    response->setHeader("Access-Control-Allow-Credentials", "true");
+    data->response->setHeader("Access-Control-Allow-Origin", data->request->headerDefault("Origin", "*"));
+    data->response->setHeader("Access-Control-Allow-Credentials", "true");
 
-    if (request->method() == "OPTIONS")
+    if (data->request->method() == "OPTIONS")
     {
         // Pre-flight request, send additional headers
-        response->setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
-        response->setHeader("Access-Control-Max-Age", "3600");
-        response->setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
-        response->setStatus(HttpStatus::Ok);
+        data->response->setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+        data->response->setHeader("Access-Control-Max-Age", "3600");
+        data->response->setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+        data->response->setStatus(HttpStatus::Ok);
     }
 
     return HttpPromise::resolve(data);
--- src/httpServer/middleware/verifyJson.cpp
+++ src/httpServer/middleware/verifyJson.cpp
@@ -6,7 +6,7 @@ namespace middleware
 HttpPromise verifyJson(std::shared_ptr<HttpData> data)
 {
     if (data->request->mimeType().compare("application/json", Qt::CaseInsensitive) != 0)
-        throw HttpException(HttpStatus::BadRequest, "Request body content type must be application/json")
+        throw HttpException(HttpStatus::BadRequest, "Request body content type must be application/json");
 
     return HttpPromise::resolve(data);
 }
