c21799c3844440831ad1e18f783f440b0b245184
Addison Elliott <addisone@becs.com>
2021-01-05 10:58:18 -0600

    Flush immediately

--- src/httpServer/httpResponse.cpp
+++ src/httpServer/httpResponse.cpp
@@ -299,6 +299,8 @@ bool HttpResponse::writeChunk(QTcpSocket *socket)
         return true;
     }
 
+    socket->flush();
+
     // Increment the bytes written, if we wrote the entire buffer, return true, otherwise return false
     writeIndex += bytesWritten;
     if (writeIndex >= buffer.size() - 1)
